f67d1f85118f7e9fc26cd005fc4d4803aea0869b
zachMelody <zhoulv2012@hotmail.com>
2022-02-10 15:55:35 +0800

    add build utils

--- README.md
+++ README.md
@@ -26,13 +26,13 @@ yarn install
 - 运行
 
 ```bash
-yarn serve
+yarn run dev
 ```
 
 - 打包
 
 ```bash
-yarn build
+yarn run build
 ```
 
 This template should help get you started developing with Vue 3 and Typescript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import WindiCSS from 'vite-plugin-windicss';
+import { loadEnv } from './src/utils/viteBuild';
 
 const resolve = (dir: string) => path.join(__dirname, dir);
+
+const { VITE_PORT, VITE_OPEN, VITE_PUBLIC_PATH } = loadEnv();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +17,10 @@ export default defineConfig({
       comps: resolve('src/components'),
       store: resolve('src/store'),
     },
+  },
+  base: process.env.NODE_ENV === 'production' ? VITE_PUBLIC_PATH : './',
+  server: {
+    port: VITE_PORT,
+    open: VITE_OPEN,
   },
 });
