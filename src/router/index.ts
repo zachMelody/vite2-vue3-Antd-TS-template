77113d608d926865460737733e816510d448da5d
zachMelody <zhoulv2012@hotmail.com>
2022-02-10 19:52:37 +0800

    fix ghpage 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from 'vue-router';
 import type { App } from 'vue';
 
 import Home from '@/pages/home/index.vue';
 import BasicLayout from '@/layout/BasicLayout.vue';
 import BlankLayout from '@/layout/BlankLayout.vue';
+
+// only githubpages preview site used, if use template please remove this check
+// and use `createWebHistory` is recommend
+const hasGithubPages = import.meta.env.VITE_GHPAGES;
 
 // 路由表
 const routes = [
@@ -64,7 +72,7 @@ const router = createRouter({
   // createWebHistory (history路由 HTML5 模式 推荐，需要服务器配置支持)
   // createMemoryHistory 带缓存 history 路由
   // 添加baseUrl，createWebHistory(baseUrl)
-  history: createWebHistory(),
+  history: hasGithubPages ? createWebHashHistory() : createWebHistory(),
   routes,
 });
 
